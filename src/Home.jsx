77909b46ba0ef8790661ef02832a3e2fbe1c1d8c
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-08-24 00:30:17 +0530

    Header

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,14 @@
 import { SignInButton } from '@clerk/clerk-react';
 import React from 'react'
 import { Button } from './components/ui/button';
+import Header from './components/Header';
 
 function Home() {
   return (
     <div>
-      <SignInButton mode='modal' forceRedirectUrl='/'>
-        <Button>SignIn</Button>
-      </SignInButton>
+      {/* Header */}
+        <Header />
+      {/* Home */}
     </div>
   )
 }
