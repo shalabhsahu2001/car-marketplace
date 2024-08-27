866d31fe75c93c2088c153740b057ce2e0c48537
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-08-28 01:16:00 +0530

    footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Category from './components/Category';
 import MostSearchedCar from './components/MostSearchedCar';
+import InfoSection from './components/InfoSection';
+import Footer from './components/Footer';
 
 function Home() {
   return (
@@ -17,6 +19,10 @@ function Home() {
         <Category />
       {/* most searched car */}
         <MostSearchedCar />
+      {/* InfoSection */}
+        <InfoSection />
+      {/*Footer*/}
+        <Footer />
     </div>
   )
 }
