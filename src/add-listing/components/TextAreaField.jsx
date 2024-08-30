248688fb931ff6547bb22ee90af48e41f5651136
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-08-30 15:58:10 +0530

    Car add-Listing component

---
+++ src/add-listing/components/TextAreaField.jsx
@@ -0,0 +1,12 @@
+import { Textarea } from "@/components/ui/textarea"
+import React from 'react'
+
+function TextAreaField({item}) {
+  return (
+    <div>
+      <Textarea/>
+    </div>
+  )
+}
+
+export default TextAreaField
--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,10 +1,44 @@
 import Header from '@/components/Header'
 import React from 'react'
+import carDetails from "./../Shared/carDetails.json";
+import InputField from './components/InputField';
+import DropdownField from './components/DropdownField';
+import { Textarea } from '@/components/ui/textarea';
+import { Separator } from '@/components/ui/separator';
 
 function AddListing() {
   return (
     <div>
       <Header />
+      <div className='px-10 md:px-20 my-10'>
+        <h2 className='font-bold text-4xl'>Add new listing</h2>
+        <form className='p-10 border rounded-xl mt-10'>
+          {/* Car detail  */}
+          <div>
+            <h2 className='font-medium text-xl mb-6'>
+              Car details
+            </h2>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+              {carDetails.carDetails.map((item, index) => (
+                <div key = {index}>
+                  <label className='text-sm'>{item?.label} {item.required && <span className='text-red-500'>*</span>}</label>
+                  {item.fieldType == 'text' || item.fieldType == 'number' 
+                  ? <InputField item={item}/> 
+                  :item.fieldType == 'dropdown' ? <DropdownField item={item}/>
+                  :item.fieldType == 'textarea' ? <Textarea item={item}/>
+                  :null}
+                </div>
+              ))}
+            </div>
+          </div>
+          <Separator className='my-6'/>
+          {/* Features list  */}
+          <div>
+            <h2 className='font-medium text-xl my-6'>Features</h2>
+          </div>
+          {/* Car images  */}
+        </form>
+      </div>
     </div>
   )
 }
